0a8ba0e467f429c421f171d4f6980eb4610db328
Dmitry Kashitsyn <korvin@deeptown.org>
2018-12-14 16:10:00 +0700

    Adds basic utxo verification

--- runtime/src/utxo.rs
+++ runtime/src/utxo.rs
@@ -1,11 +1,75 @@
 use primitives::H256;
-use srml_support::{StorageValue, dispatch::{Vec, Result}};
+use rstd::collections::btree_map::BTreeMap;
+use runtime_io::ed25519_verify;
+use runtime_primitives::traits::{BlakeTwo256, Hash};
+use srml_support::{
+	dispatch::{Result, Vec},
+	StorageMap,
+};
+use system::ensure_inherent;
 
 pub trait Trait: system::Trait {}
 
 decl_module! {
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-		fn spend(transaction: Transaction) -> Result {
+		pub fn execute(origin, transaction: Transaction) -> Result {
+			ensure_inherent(origin)?;
+
+			// In order to execute the transaction we need to ensure that:
+			// - all inputs match to unspent outputs
+			// - each unspent output is used exactly once
+			// - each output is defined exactly once
+			// - sum of output value is less than sum of input value
+			// - provided signatures must be valid
+
+			let inputs_valid = transaction.inputs
+				.iter()
+				.map(|input| BlakeTwo256::hash_of(input))
+				.all(|hash| <UnspentOutputs<T>>::exists(&hash));
+			ensure!(inputs_valid, "all inputs must exist and be unspent");
+
+			{
+				let input_set: BTreeMap<_, ()> = transaction.inputs
+					.iter()
+					.map(|input| (input, ()))
+					.collect();
+				ensure!(input_set.len() == transaction.inputs.len(), "each input must be used only once");
+			}
+
+			{
+				let output_set: BTreeMap<_, ()> = transaction.outputs
+					.iter()
+					.map(|output| (output, ()))
+					.collect();
+				ensure!(output_set.len() == transaction.outputs.len(), "each output must be defined only once");
+			}
+
+			let input_value: u128 = transaction.inputs
+				.iter()
+				.map(|input| <UnspentOutputs<T>>::get(&input.linked_output).value)
+				.fold(Ok(0u128), |sum, value| {
+					sum.and_then(|s|
+						match s.checked_add(value) {
+							Some(sum) => Ok(sum),
+							None => Err("input value overflow"),
+						}
+					)
+				})?;
+
+			let output_value: u128 = transaction.outputs
+				.iter()
+				.map(|output| output.value)
+				.fold(Ok(0u128), |sum, value| {
+					sum.and_then(|s|
+						match s.checked_add(value) {
+							Some(sum) => Ok(sum),
+							None => Err("output value overflow"),
+						}
+					)
+				})?;
+
+			ensure!(input_value >= output_value, "output value must not exceed input value");
+
 			unimplemented!()
 		}
 	}
@@ -17,30 +81,27 @@ decl_storage! {
 	}
 }
 
-#[derive(Debug, PartialEq, Default, Clone, Encode, Decode)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Default, Clone, Encode, Decode, Hash)]
 pub struct Transaction {
 	pub inputs: Vec<TransactionInput>,
 	pub outputs: Vec<TransactionOutput>,
-	pub lock_time: u32,
 }
 
-#[derive(Debug, PartialEq, Default, Clone, Encode, Decode)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Default, Clone, Encode, Decode, Hash)]
 pub struct TransactionInput {
-	pub previous_output: OutPoint,
+	pub linked_output: H256,
 	pub signature: Bytes,
 }
 
-#[derive(Debug, Default, PartialEq, Clone, Encode, Decode)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
+#[derive(Default, PartialEq, Eq, PartialOrd, Ord, Clone, Encode, Decode, Hash)]
 pub struct TransactionOutput {
-	pub value: u64,
+	pub value: u128,
 	pub pubkey: Bytes,
 }
 
-#[derive(Debug, PartialEq, Eq, Clone, Default, Encode, Decode)]
-pub struct OutPoint {
-	pub hash: H256,
-	pub index: u32,
-}
-
-#[derive(Default, Debug, PartialEq, Clone, Eq, Hash, Encode, Decode)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
+#[derive(Default, PartialEq, Eq, PartialOrd, Ord, Clone, Encode, Decode, Hash)]
 pub struct Bytes(Vec<u8>);
