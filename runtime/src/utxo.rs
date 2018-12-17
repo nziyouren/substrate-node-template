ad8aa82d2e76b6da5e4c6d58a632b60bb88a0279
Dmitry Kashitsyn <korvin@deeptown.org>
2018-12-17 12:56:47 +0700

    Refactors the module

--- runtime/src/utxo.rs
+++ runtime/src/utxo.rs
@@ -7,77 +7,6 @@ use system::ensure_inherent;
 
 pub trait Trait: system::Trait {}
 
-decl_module! {
-	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-		pub fn execute(origin, transaction: Transaction) -> Result {
-			ensure_inherent(origin)?;
-
-			// In order to execute the transaction we need to ensure that:
-			// - all inputs match to existing and unspent outputs
-			// - each unspent output is used exactly once
-			// - each output is defined exactly once
-			// - sum of output value is less than sum of input value
-			// - provided signatures must be valid
-
-			{
-				let input_set: BTreeMap<_, ()> = transaction.inputs
-					.iter()
-					.map(|input| (input, ()))
-					.collect();
-				ensure!(input_set.len() == transaction.inputs.len(), "each input must be used only once");
-			}
-
-			{
-				let output_set: BTreeMap<_, ()> = transaction.outputs
-					.iter()
-					.map(|output| (output, ()))
-					.collect();
-				ensure!(output_set.len() == transaction.outputs.len(), "each output must be defined only once");
-			}
-
-			let input_value: u128 = transaction.inputs
-				.iter()
-				.map(|input| (input.signature, Self::utxo(&input.linked_output)))
-				.fold(Ok(0u128), |sum, (signature, output)| {
-					ensure!(output.is_some(), "all linked outputs must exist and be unspent");
-					let output = output.unwrap();
-
-					let digest = output.encode();
-					ensure!(ed25519_verify(signature.as_fixed_bytes(), &digest, &output.pubkey), "signature must be valid");
-
-					sum.and_then(|s|
-						match s.checked_add(output.value) {
-							Some(sum) => Ok(sum),
-							None => Err("input value overflow"),
-						}
-					)
-				})?;
-
-			let output_value: u128 = transaction.outputs
-				.iter()
-				.map(|output| output.value)
-				.fold(Ok(0u128), |sum, value| {
-					sum.and_then(|s|
-						match s.checked_add(value) {
-							Some(sum) => Ok(sum),
-							None => Err("output value overflow"),
-						}
-					)
-				})?;
-
-			ensure!(input_value >= output_value, "output value must not exceed input value");
-
-			unimplemented!()
-		}
-	}
-}
-
-decl_storage! {
-	trait Store for Module<T: Trait> as Demo {
-		UnspentOutputs get(utxo): map H256 => Option<TransactionOutput>;
-	}
-}
-
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
 #[derive(PartialEq, Eq, PartialOrd, Ord, Default, Clone, Encode, Decode, Hash)]
 pub struct Transaction {
@@ -97,4 +26,88 @@ pub struct TransactionInput {
 pub struct TransactionOutput {
 	pub value: u128,
 	pub pubkey: H256,
+}
+
+decl_module! {
+	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+		pub fn execute(origin, transaction: Transaction) -> Result {
+			ensure_inherent(origin)?;
+
+			Self::check_transaction(&transaction)?;
+			Self::spend_transaction(transaction)?;
+
+			Ok(())
+		}
+	}
+}
+
+impl<T: Trait> Module<T> {
+	fn check_transaction(transaction: &Transaction) -> Result {
+		// We need to ensure that:
+		// - all inputs match to existing and unspent outputs
+		// - each unspent output is used exactly once
+		// - each output is defined exactly once
+		// - sum of output value is less than sum of input value
+		// - provided signatures are valid
+
+		{
+			let input_set: BTreeMap<_, ()> = transaction.inputs
+				.iter()
+				.map(|input| (input, ()))
+				.collect();
+			ensure!(input_set.len() == transaction.inputs.len(), "each input must be used only once");
+		}
+
+		{
+			let output_set: BTreeMap<_, ()> = transaction.outputs
+				.iter()
+				.map(|output| (output, ()))
+				.collect();
+			ensure!(output_set.len() == transaction.outputs.len(), "each output must be defined only once");
+		}
+
+		let input_value: u128 = transaction.inputs
+			.iter()
+			.map(|input| (input.signature, Self::utxo(&input.linked_output)))
+			.fold(Ok(0u128), |sum, (signature, output)| {
+				ensure!(output.is_some(), "all linked outputs must exist and be unspent");
+				let output = output.unwrap();
+
+				let digest = output.encode();
+				ensure!(ed25519_verify(signature.as_fixed_bytes(), &digest, &output.pubkey), "signature must be valid");
+
+				sum.and_then(|s|
+					match s.checked_add(output.value) {
+						Some(sum) => Ok(sum),
+						None => Err("input value overflow"),
+					}
+				)
+			})?;
+
+		let output_value: u128 = transaction.outputs
+			.iter()
+			.map(|output| output.value)
+			.fold(Ok(0u128), |sum, value| {
+				sum.and_then(|s|
+					match s.checked_add(value) {
+						Some(sum) => Ok(sum),
+						None => Err("output value overflow"),
+					}
+				)
+			})?;
+
+		ensure!(input_value >= output_value, "output value must not exceed input value");
+
+		Ok(())
+	}
+
+	fn spend_transaction(transaction: Transaction) -> Result {
+		Ok(())
+	}
+}
+
+decl_storage! {
+	trait Store for Module<T: Trait> as Demo {
+		UnspentOutputs get(utxo): map H256 => Option<TransactionOutput>;
+	}
 }
