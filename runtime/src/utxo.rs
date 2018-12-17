9ad81bb30f0335ca137c82bb8f4ed62efc3d5e0c
Dmitry Kashitsyn <korvin@deeptown.org>
2018-12-17 14:47:52 +0700

    Adds `spend_transaction` logic

--- runtime/src/utxo.rs
+++ runtime/src/utxo.rs
@@ -1,8 +1,11 @@
-use parity_codec::Encode;
 use primitives::{Signature, H256};
 use rstd::collections::btree_map::BTreeMap;
 use runtime_io::ed25519_verify;
-use srml_support::dispatch::{Result, Vec};
+use runtime_primitives::traits::{BlakeTwo256, Hash};
+use srml_support::{
+	dispatch::{Result, Vec},
+	StorageMap,
+};
 use system::ensure_inherent;
 
 pub trait Trait: system::Trait {}
@@ -41,6 +44,12 @@ decl_module! {
 	}
 }
 
+decl_storage! {
+	trait Store for Module<T: Trait> as Demo {
+		UnspentOutputs get(utxo): map H256 => Option<TransactionOutput>;
+	}
+}
+
 impl<T: Trait> Module<T> {
 	fn check_transaction(transaction: &Transaction) -> Result {
 		// We need to ensure that:
@@ -55,6 +64,7 @@ impl<T: Trait> Module<T> {
 				.iter()
 				.map(|input| (input, ()))
 				.collect();
+
 			ensure!(input_set.len() == transaction.inputs.len(), "each input must be used only once");
 		}
 
@@ -63,51 +73,64 @@ impl<T: Trait> Module<T> {
 				.iter()
 				.map(|output| (output, ()))
 				.collect();
+
 			ensure!(output_set.len() == transaction.outputs.len(), "each output must be defined only once");
 		}
 
-		let input_value: u128 = transaction.inputs
-			.iter()
-			.map(|input| (input.signature, Self::utxo(&input.linked_output)))
-			.fold(Ok(0u128), |sum, (signature, output)| {
-				ensure!(output.is_some(), "all linked outputs must exist and be unspent");
-				let output = output.unwrap();
+		let total_input: u128 = transaction.inputs.iter().fold(Ok(0u128), |sum, input| {
+			// Fetch UTXO from the storage
+			let output = match Self::utxo(&input.linked_output) {
+				Some(output) => output,
+				None => return Err("all linked outputs must exist and be unspent"),
+			};
 
-				let digest = output.encode();
-				ensure!(ed25519_verify(signature.as_fixed_bytes(), &digest, &output.pubkey), "signature must be valid");
+			// Check that we're authorized to use it
+			ensure!(
+				ed25519_verify(
+					input.signature.as_fixed_bytes(),
+					input.linked_output.as_fixed_bytes(),
+					&output.pubkey
+				),
+				"signature must be valid"
+			);
 
-				sum.and_then(|s|
-					match s.checked_add(output.value) {
-						Some(sum) => Ok(sum),
-						None => Err("input value overflow"),
-					}
-				)
-			})?;
+			// Add the value to the incoming sum
+			sum.and_then(|s| match s.checked_add(output.value) {
+				Some(sum) => Ok(sum),
+				None => Err("input value overflow"),
+			})
+		})?;
 
-		let output_value: u128 = transaction.outputs
-			.iter()
-			.map(|output| output.value)
-			.fold(Ok(0u128), |sum, value| {
-				sum.and_then(|s|
-					match s.checked_add(value) {
-						Some(sum) => Ok(sum),
-						None => Err("output value overflow"),
-					}
-				)
-			})?;
+		let total_output: u128 = transaction.outputs.iter().map(|output| output.value).fold(
+			Ok(0u128),
+			|sum, value| {
+				sum.and_then(|s| match s.checked_add(value) {
+					Some(sum) => Ok(sum),
+					None => Err("output value overflow"),
+				})
+			},
+		)?;
 
-		ensure!(input_value >= output_value, "output value must not exceed input value");
+		ensure!(total_input >= total_output, "output value must not exceed input value");
 
 		Ok(())
 	}
 
 	fn spend_transaction(transaction: Transaction) -> Result {
-		Ok(())
-	}
-}
+		// In order to spend transaction we need to:
+		// - remove used UTXO's
+		// - add new UTXO's created by this transaction
 
-decl_storage! {
-	trait Store for Module<T: Trait> as Demo {
-		UnspentOutputs get(utxo): map H256 => Option<TransactionOutput>;
+		for input in transaction.inputs {
+			<UnspentOutputs<T>>::remove(input.linked_output);
+		}
+
+		for output in transaction.outputs {
+			let hash = BlakeTwo256::hash_of(&output);
+			ensure!(!<UnspentOutputs<T>>::exists(hash), "UTXO already exists"); // FIXME
+			<UnspentOutputs<T>>::insert(hash, output);
+		}
+
+		Ok(())
 	}
 }
